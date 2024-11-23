7dc502dbd7b9f2044cc7b69ec6805dfcd85355b1
sunday00 <kind65505256@gmail.com>
2024-11-23 18:07:12 +0900

    update ip

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ cp -r .next/static .next/standalone/.next/
 cp -r public .next/standalone/
 
 echo 'rsync to main folder'
-rsync -az .next/standalone sunday00@211.184.119.6:/volume1/web/hobby-log/frontend/html/
+rsync -az .next/standalone sunday00@211.184.116.232:/volume1/web/hobby-log/frontend/html/
 
 echo 'rsync for restart'
-touch deploy.time && date > deploy.time && rsync -az deploy.time sunday00@211.184.119.6:/volume1/web/hobby-log/frontend/html/deploy-time/
+touch deploy.time && date > deploy.time && rsync -az deploy.time sunday00@211.184.116.232:/volume1/web/hobby-log/frontend/html/deploy-time/
 
 rm -rf deploy.time
 echo "
